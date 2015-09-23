693be4f6a49cc05f6b292dcb44ff4191d1d6f95e
johndcna <decenajac@gmail.com>
2015-09-23 19:29:39 +0800

    modified the script.js

--- js/script.js
+++ js/script.js
@@ -15,32 +15,74 @@
                 });
             }]);
 
-
-        var myController = module.controller("LoginController", function($scope,$http) {
+        var controllers = {}; 
+ 			
+ 			controllers.LoginController = function($scope,$http) {
         
-	        $scope.myData = {};
-				
-			$scope.login = function() {
-				alert("click");
-			};
+		        $scope.myData = {};
+					
+				$scope.test = function() {
+					alert("click");
+				}
 
-			$scope.myData.doClick = function(item, event) {
-
-	        		var responsePromise = $http.get("json-test-data.json");
-
-	        		responsePromise.success(function(data, status, headers, config) {
-	           				if (typeof(Storage) !== "undefined") {
-	 							// Store
-	    						localStorage.setItem("jsonLogin", JSON.stringify(data));
-	    						window.location.href = "#/main";
-	    						alert("sud");
-							} else {
-	  							alert("not supported");
-							}
-	            	});
-	                responsePromise.error(function(data, status, headers, config) {
-	                    alert("AJAX failed!");
-	                });
+				$scope.myData.doClick = function(item, event) {
+							/* for information
+		           					var inputData = '{"username": "'+$scope.username+'", "password": "'+$scope.password+'"}';
+		        		 			$http.post("InformatronYX/informatron/user/login",inputData);
+										if(data.token !=null) {
+												if (typeof(Storage) !== "undefined") {
+		 											// Store
+		    										localStorage.setItem("jsonLogin", JSON.stringify(data));
+		    										window.location.href = "#/main";
+		    										alert(str);
+												} 
+												else {
+		  											alert("not supported");
+												}
+										}
+										else {
+												alert("Invalid username or password.");
+										}
+		           				*/
+		        		var responsePromise = $http.get("json-test-data.json");
+		        		responsePromise.success(function(data, status, headers, config) {
+		           				
+		           				if (typeof(Storage) !== "undefined") {
+		 							// Store
+		    						localStorage.setItem("jsonLogin", JSON.stringify(data));
+		    						window.location.href = "#/main";
+		    						alert("sd");
+								} else {
+		  							alert("not supported");
+								}
+		            	});
+		                responsePromise.error(function(data, status, headers, config) {
+		                    alert("AJAX failed!");
+		                });
+	        	}
         	}
-    	});
-            
+		module.controller(controllers); 
+
+		/*
+var _getAllFilesFromFolder = function(dir) {
+
+    var filesystem = require("fs");
+    var results = [];
+
+    filesystem.readdirSync(dir).forEach(function(file) {
+
+        file = dir+'/'+file;
+        var stat = filesystem.statSync(file);
+
+        if (stat && stat.isDirectory()) {
+            results = results.concat(_getAllFilesFromFolder(file))
+        } else results.push(file);
+
+    });
+
+    return results;
+
+};
+		*/
+
+ 
