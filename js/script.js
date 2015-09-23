14e47d399ef6147b9d563667519463c46b5d6789
Albert Dale Palacio <palacioalbertdale@gmail.com>
2015-09-23 15:39:56 +0800

    partial style 2 jumbotrons and login routing fixed

--- js/script.js
+++ js/script.js
@@ -1,17 +1,46 @@
-var $ = jQuery.noConflict();
-
-
-
-// Progress Bar
-
-$(document).ready(function ($) {
-    "use strict";
-    
-    $('.skill-shortcode').appear(function () {
-        $('.progress').each(function () {
-            $('.progress-bar').css('width',  function () { return ($(this).attr('data-percentage') + '%')});
-        });
-    }, {accY: -100});
+		var module = angular.module("LOPapp", ['ngRoute']);
         
+        module.config( ['$routeProvider', function($routeProvider) {
+            $routeProvider
+            	.when('/',{
+            		templateUrl:'main/login.html',
+            		controller: 'LoginController'
+            	})
+            	.when('/main',{
+            		templateUrl:'main/main.html',
+            		controller: 'LoginController'
+            	})
+            	.otherwise({
+                   	redirectTo: '/'
+                });
+            }]);
+
+
+        var myController = module.controller("LoginController", function($scope,$http) {
         
-});
+	        $scope.myData = {};
+				
+			$scope.login = function() {
+				alert("click");
+			};
+
+			$scope.myData.doClick = function(item, event) {
+
+	        		var responsePromise = $http.get("json-test-data.json");
+
+	        		responsePromise.success(function(data, status, headers, config) {
+	           				if (typeof(Storage) !== "undefined") {
+	 							// Store
+	    						localStorage.setItem("jsonLogin", JSON.stringify(data));
+	    						window.location.href = "#/main";
+	    						alert("sud");
+							} else {
+	  							alert("not supported");
+							}
+	            	});
+	                responsePromise.error(function(data, status, headers, config) {
+	                    alert("AJAX failed!");
+	                });
+        	}
+    	});
+            
