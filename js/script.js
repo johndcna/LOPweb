405464c34e2443e5f6e91584bc4185562d030f7b
prongssss <jvsuson17@gmail.com>
2015-09-30 16:29:13 +0800

    displays text and picture
    naa koy gi add sa json data nga "fileExtension" kay para maka display ko
    sa data. Palihog sad kog tarung sa UI. Magpush lang niya ko ug kanang
    maka display na ug video and audio

--- js/script.js
+++ js/script.js
@@ -26,24 +26,35 @@
 				}
 
 				$scope.myData.doClick = function(item, event) {
-							/* for information
-		           					var inputData = '{"username": "'+$scope.username+'", "password": "'+$scope.password+'"}';
-		        		 			$http.post("InformatronYX/informatron/user/login",inputData);
-										if(data.token !=null) {
-												if (typeof(Storage) !== "undefined") {
-		 											// Store
-		    										localStorage.setItem("jsonLogin", JSON.stringify(data));
-		    										window.location.href = "#/main";
-		    										alert(str);
-												} 
-												else {
-		  											alert("not supported");
-												}
-										}
-										else {
-												alert("Invalid username or password.");
-										}
-		           				*/
+							// for information
+		           					/*var inputData =  {
+		           						'username' : $scope.username,
+		           						'password' : $scope.password
+		           					};
+		           					
+		           					alert(inputData);
+		        		 			var responsePromise =  $http.post("http://192.168.254.101:8080/InformatronYX/informatron/user/login",inputData);//http://192.168.254.101:8080/InformatronYX/informatron/user/login
+										responsePromise.success(function(response) {
+											if(response.token !=null) {
+													if (typeof(Storage) !== "undefined") {
+			 											// Store
+			    										localStorage.setItem("jsonLogin", JSON.stringify(response));
+			    										window.location.href = "#/main";
+			    										alert(str);
+													} 
+													else {
+			  											alert("not supported");
+													}
+											}
+											else {
+													alert("Invalid username or password.");
+											}
+										});
+										 responsePromise.error(function(response) {
+					                   		alert("AJAX failed!");
+					                		});*/
+								
+		           				
 		        		var responsePromise = $http.get("json-test-data.json");
 		        		responsePromise.success(function(data, status, headers, config) {
 		           				if(data.token !=null) {
@@ -80,9 +91,12 @@
 						$scope.flag = true;
 					}
 				}
-				$scope.displayLO = function(LOid){
-					alert(""+LOid);// ng-include="main.html"
+				$scope.displayLO = function(LOsequence){
+					//alert(""+LOid);// ng-include="main.html"
+					var fileName;
+					var fileExtension;
 					$("#mainDiv").empty();
+					//alert(LOsequence);
 					/*
 							for()
 							{
@@ -90,9 +104,71 @@
 								
 							}
 					*/
-				//	$("#mainDiv").prepend("<div ng-include data-src='/main/main.html'>"+LOid+"</div>");
-				//	$("#mainDiv").prepend("<h2>Spectacular Mountain</h2><img src='C:\\Users\\John\\Pictures\\2014-12-31\\john.png' alt='Mountain View' style='width:304px;height:228px;'>");
+					//$("#mainDiv").prepend("<div ng-include data-src='/main/main.html'>"+LOid+"</div>");
+					//$("#mainDiv").prepend("<h2>Spectacular Mountain</h2><img src='C:\\Users\\John\\Pictures\\2014-12-31\\john.png' alt='Mountain View' style='width:304px;height:228px;'>");
+					/*if(LOid == 0)
+					{
+						$("#mainDiv").append("<object width='300' height='300' type='text/plain' data='le/idm key.txt' border='0'>");
+					}
+					else if(LOid == 1)
+					{
+						$("#mainDiv").append("<img src='le/001.jpg' alt='Mountain View' style='width:304px;height:228px;'>")
+					}
+					else if(LOid == 2)
+					{
+						$("#mainDiv").append("<object width='300' height='300' type='text/plain' data='le/idm key.txt' border='0'>");
+						$("#mainDiv").append("<br>");
+						$("#mainDiv").append("<img src='le/001.jpg' alt='Mountain View' style='width:304px;height:228px;'>")
+					}*/
+					//alert($scope.data.id + " " + $scope.data.username);
+					//for(int i = 0; i < 3; i++)
+					//{
+						//var arr = $scope.data.liableLearningObjects[LOid].sequence;
+						//alert(arr.title);
+						//for(int i = 0; i < $scope.data.liableLearningObjects.length; i++)
+						//{
 
+						//}
+						/*$.each($scope.data.liableLearningObjects, function(i,j)
+						{
+							//alert(j.id + ": " + j.title);
+							if(j.id == LOid)
+							{
+								q = j.sequence;
+							}
+						});
+						alert(q.length);
+						$.each(q, function(a,b)
+						{
+							alert(a+":"+b.title);
+						});*/
+
+					//}
+					$.each(LOsequence, function(a,le)
+					{
+						fileName = le.id;
+						fileExtension = le.fileExtension;
+						//alert(fileName);
+						if(le.type == "text")
+						{
+							//alert("text le");
+							$("#mainDiv").append("<object width='300' height='300' type='text/plain' data='le/"+fileName+fileExtension+"' border='0'>");
+						}
+						else if(le.type == "picture")
+						{
+							//alert("picture le");
+							$("#mainDiv").append("<img src='le/"+fileName+fileExtension+"'"+ " alt='Mountain View' style='width:304px;height:228px;'>");
+						}
+						else if(le.type == "video")
+						{
+							alert("video le");
+						}
+						else if(le.type == "audio")
+						{
+							alert("audio le");
+						}
+						$("#mainDiv").append("<br>");
+					});
 				}
 
         	}
